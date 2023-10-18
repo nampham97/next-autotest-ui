1860ffd2225616ed7d9a2c52f2b485af35e650d8
Nampd <nampham97se@gmail.com>
2023-10-19 01:21:11 +0700

    first ui

---
+++ src/app/category/page.js
@@ -0,0 +1,11 @@
+'use client';
+import Category from '../components/category/Category';
+import Layout from '../components/layout/Layout';
+
+export default function categoryPage() {
+  return (
+    <Layout title='Category'>
+        <Category />
+    </Layout>
+  )
+}
---
+++ src/app/sub-category/page.js
@@ -0,0 +1,12 @@
+'use client';
+import Layout from '../components/layout/Layout';
+import SubCategory from '../components/sub-category/SubCategory';
+
+
+export default function subCategoryPage() {
+  return (
+    <Layout title='Sub-Category'>
+        <SubCategory />
+    </Layout>
+  )
+}
